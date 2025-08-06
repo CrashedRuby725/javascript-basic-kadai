ecab74b4411307b8eb91a7410afe9a0cfa21528c
CrashedRuby725 <crashed.ruby@rosental.jp>
2025-08-06 14:54:36 +0900

    Update bmi.js

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,9 +1,10 @@
 // 定数の宣言
 const userWeight = 68;
-const userHeight = 1.59;
+const userHeight = 1.7;
 
 // 変数の宣言
 let bmi = userWeight / ( userHeight * userHeight );
 
 // コンソールへの出力
 console.log(bmi);
+
